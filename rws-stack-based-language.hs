45c7c25a4472ef862b04cc3db71e0c5b7c0ad7d6
Robert Bendun <robert@bendun.cc>
2021-11-03 03:52:29 +0100

    Concatenative >>> Applicative

--- rws-stack-based-language.hs
+++ rws-stack-based-language.hs
@@ -2,6 +2,8 @@ import Control.Monad
 import Control.Monad.Trans.RWS.Lazy
 import Data.Char
 
+-- 😂Concatenative Functional Languages >>> Haskell
+-- https://evincarofautumn.blogspot.com/2012/02/why-concatenative-programming-matters.html
 (.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (.:) = (.) . (.)
 
